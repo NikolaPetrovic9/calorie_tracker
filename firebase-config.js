713d75a9898fffad1663e7664bd049a1f8b8ceb2
NikolaPetrovic9 <NikolaPetrovic9@users.noreply.github.com>
2026-02-03 12:53:25 +0000

    deploy: af9871c2b168159bf6db9e5b95c07e3c8c163b1f

---
+++ firebase-config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyC7Lgquk2mvgxqOwS0G0MXMJiD4jzOKbTk",
+  authDomain: "colorie-calculator.firebaseapp.com",
+  databaseURL: "https://colorie-calculator-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "colorie-calculator",
+  storageBucket: "colorie-calculator.firebasestorage.app",
+  messagingSenderId: "113528951260",
+  appId: "1:113528951260:web:2b7cde7a5a8b9509a07f19",
+  measurementId: "G-3TPB1H1KM0"
+};
